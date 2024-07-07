eecd43d77e207fdb72d04d74b63ee3d281492a45
Макар Павлович <106832520+mokinprokin@users.noreply.github.com>
2024-07-07 21:30:08 +0600

    Delete AkademyShop/shop directory

--- AkademyShop/shop/editor/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Item(models.Model):
-    name=models.TextField()
-    count=models.IntegerField()
-    cost=models.IntegerField()
-    description=models.TextField()
-# Create your models here.
